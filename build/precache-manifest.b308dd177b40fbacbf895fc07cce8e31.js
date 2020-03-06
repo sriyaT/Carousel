eb67138bb580b63cdbea2513a268116642cee003
sriyaT <tsriya6@gmail.com>
2020-03-07 00:26:27 +0530

    first commits

---
+++ build/precache-manifest.b308dd177b40fbacbf895fc07cce8e31.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a235f9767f47d23b7fad151af2b498f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7129958bea02f7e14a64",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "bc2e5e60a878605ae0fa",
+    "url": "/static/js/2.4bf6385f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.4bf6385f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7129958bea02f7e14a64",
+    "url": "/static/js/main.db8ef0b1.chunk.js"
+  },
+  {
+    "revision": "5a2918a409994b2e900d",
+    "url": "/static/js/runtime-main.a3e2fd79.js"
+  },
+  {
+    "revision": "201a343ca5cc34b6e045941093610d91",
+    "url": "/static/media/happersBazaar.201a343c.jpg"
+  },
+  {
+    "revision": "920002587f9f2759633236c6f99e40bc",
+    "url": "/static/media/paris.92000258.jpeg"
+  },
+  {
+    "revision": "4ad2099f03eba3763e921d29cbeb80a6",
+    "url": "/static/media/switzerland.4ad2099f.jpeg"
+  }
+]);
