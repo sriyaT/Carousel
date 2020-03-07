d53ef2ef6e458ab837d0dd65f3940f0381817743
sriyaT <tsriya6@gmail.com>
2020-03-07 10:05:34 +0530

    Updates

---
+++ precache-manifest.57b0a40844d562a5d4ee567cafa3b22d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9d2e148d45961162365cb7601c919e7",
+    "url": "/sriyaT/Carousel/index.html"
+  },
+  {
+    "revision": "5b7774e6b23477dd5251",
+    "url": "/sriyaT/Carousel/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "bc2e5e60a878605ae0fa",
+    "url": "/sriyaT/Carousel/static/js/2.4bf6385f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/sriyaT/Carousel/static/js/2.4bf6385f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b7774e6b23477dd5251",
+    "url": "/sriyaT/Carousel/static/js/main.852ffd1e.chunk.js"
+  },
+  {
+    "revision": "9b1fd2cb9daefbe3ad74",
+    "url": "/sriyaT/Carousel/static/js/runtime-main.76e757bd.js"
+  },
+  {
+    "revision": "201a343ca5cc34b6e045941093610d91",
+    "url": "/sriyaT/Carousel/static/media/happersBazaar.201a343c.jpg"
+  },
+  {
+    "revision": "920002587f9f2759633236c6f99e40bc",
+    "url": "/sriyaT/Carousel/static/media/paris.92000258.jpeg"
+  },
+  {
+    "revision": "4ad2099f03eba3763e921d29cbeb80a6",
+    "url": "/sriyaT/Carousel/static/media/switzerland.4ad2099f.jpeg"
+  }
+]);
